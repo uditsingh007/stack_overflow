c9ce9318810147d0ba314d54ea333ba8aba3c470
uditsingh007 <singh.udit14@gmail.com>
2023-10-31 17:06:49 +0530

    Add: Theme provider

---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+interface ThemeContextType {
+  mode: string;
+  setMode: (mode: string) => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [mode, setMode] = useState("");
+  const handleModeChange = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setMode("light");
+      document.documentElement.classList.add("light");
+    }
+  };
+  useEffect(() => {
+    handleModeChange();
+  }, [mode]);
+  return (
+    <ThemeContext.Provider value={{ mode, setMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error("useTheme must be used withtin a theme provider");
+  }
+  return context;
+};
