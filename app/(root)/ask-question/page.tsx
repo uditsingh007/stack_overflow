642954a7122b09390cd8f06f22c110f171bae469
uditsingh007 <singh.udit14@gmail.com>
2024-01-19 16:21:45 +0530

    Added images domain for logged in user

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,12 +1,11 @@
 import Question from "@/components/forms/Question";
 import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
 
 const Page = async () => {
-  // const { userId } = auth();
-  const userId = "user123";
+  const { userId } = auth();
   if (!userId) redirect("/sign-in");
   const user = await getUserById({ userId });
   return (
