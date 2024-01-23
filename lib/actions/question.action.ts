8fa88b6ca0218bf652202478a699b9f3dc2e2d94
uditsingh007 <singh.udit14@gmail.com>
2024-01-23 15:20:49 +0530

    Add: home filters

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -21,7 +21,7 @@ import { FilterQuery } from "mongoose";
 export const getQuestions = async (params: GetQuestionsParams) => {
   try {
     connectToDatabase();
-    const { searchQuery } = params;
+    const { searchQuery, filter } = params;
     const query: FilterQuery<typeof Question> = {};
     if (searchQuery) {
       query.$or = [
@@ -29,9 +29,26 @@ export const getQuestions = async (params: GetQuestionsParams) => {
         { content: { $regex: new RegExp(searchQuery, "i") } },
       ];
     }
+
+    let sortOptions = {};
+
+    switch (filter) {
+      case "newest":
+        sortOptions = { createdAt: -1 };
+        break;
+      case "frequent":
+        sortOptions = { views: -1 };
+        break;
+      case "unanswered":
+        query.answers = { $size: 0 };
+        break;
+      default:
+        break;
+    }
     const questions = await Question.find(query)
       .populate({ path: "tags", model: Tag })
-      .populate({ path: "author", model: User });
+      .populate({ path: "author", model: User })
+      .sort(sortOptions);
     return { questions };
   } catch (error) {
     console.log(error);
